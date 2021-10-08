b1dfe0f851e87df959e1bd22786f82b81206d946
AnnaMurko <s50755@edu.campus02.at>
2021-10-08 19:58:56 +0200

    Variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,17 @@
 public class Calculator {
     public static void main(String[] args) {
+        int var = 1;
+        System.out.println(var);
+        double var2 = 2;
+        System.out.println(var2);
+        boolean isRed = true;
+        System.out.println(isRed);
+        char cha = 's';
+        System.out.println(cha);
+        String name = "Anna";
+        System.out .println(name);
+
+
 
     }
 }
